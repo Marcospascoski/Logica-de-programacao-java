2653bc5eef20b6334793bfaa4459636eea1ccb86
Marcos Pascoski <marcospascoski@gmail.com>
2018-12-03 12:18:48 -0300

    uri1012

---
+++ src/uri1012.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1012 {
+
+	public static void main(String[] args) {
+		
+		double pi = 3.14159;
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		double c = sc.nextDouble();
+		
+		double areaTriangulo = a * c/2;
+		double areaCirculo =  pi * Math.pow(c, 2);
+		double areaTrapezio = (a + b) * c/2;
+		double areaQuadrado = b * b ;
+		double areaRetangulo = a * b; 
+		
+		System.out.printf("TRIANGULO: %.3f%n",areaTriangulo);
+		System.out.printf("CIRCULO: %.3f%n",areaCirculo);
+		System.out.printf("TRAPEZIO: %.3f%n",areaTrapezio);
+		System.out.printf("QUADRADO: %.3f%n",areaQuadrado);
+		System.out.printf("RETANGULO: %.3f%n",areaRetangulo);
+		
+		sc.close();
+	}
+
+}
